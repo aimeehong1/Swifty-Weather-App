72b0fa05de38570c8abe1099106c8b146e19e6ce
Aimee Hong <honglp@bc.edu>
2024-11-18 16:31:32 -0500

    Initial Commit

---
+++ SwiftyWeather/SwiftyWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftyWeatherApp.swift
+//  SwiftyWeather
+//
+//  Created by Aimee Hong on 11/18/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftyWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
